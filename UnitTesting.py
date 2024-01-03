74b3e7f952fcd929acf52f52b039d38af3a99e10
vegeta221 <64312436+vegeta221@users.noreply.github.com>
2024-01-03 13:21:12 +0200

    added unit testing

---
+++ UnitTesting.py
@@ -0,0 +1,46 @@
+import unittest
+from main import app
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_read_student_route(self):
+        # Test the /students
+        response = self.app.get('/students/123')
+        self.assertEqual(response.status_code, 200)
+
+    def test_read_staff_route(self):
+        response = self.app.get('/staff/3')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_student_route(self):
+        student = {
+            "name": "Jane Doe",
+            "email": "jane@example.com"
+        }
+        response = self.app.put('/students/123', json=student)
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_staff_route(self):
+        staff = {
+            "name": "Marie jane",
+            "email": "marie@example.com",
+            "position": "Doctor"
+        }
+        response = self.app.put('/staff/3', json=staff)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_student_route(self):
+        response = self.app.delete('/students/123')
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_staff_route(self):
+        response = self.app.delete('/staff/3')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
